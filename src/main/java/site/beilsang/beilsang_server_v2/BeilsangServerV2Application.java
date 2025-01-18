c23ab880034b417e9d68653a9882aba0ae4058e8
633jinn <judyg00dy68@gmail.com>
2025-01-19 02:08:21 +0900

    [Feat] 개발 초기 세팅

---
+++ src/main/java/site/beilsang/beilsang_server_v2/BeilsangServerV2Application.java
@@ -0,0 +1,13 @@
+package site.beilsang.beilsang_server_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeilsangServerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeilsangServerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/beilsang/beilsang_server_v2/BeilsangServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.beilsang.beilsang_server_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeilsangServerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
