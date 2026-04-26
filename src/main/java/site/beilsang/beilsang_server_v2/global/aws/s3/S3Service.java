cb8d8951df52107c79945bbbfe5a8a5ffbb069c7
JJadeYoon <yjs19990421@gmail.com>
2026-04-26 16:59:31 +0900

    feat: 기존 S3 업로드 메소드 제거

--- src/main/java/site/beilsang/beilsang_server_v2/global/aws/s3/S3Service.java
+++ src/main/java/site/beilsang/beilsang_server_v2/global/aws/s3/S3Service.java
@@ -33,13 +33,9 @@ public class S3Service {
     @Value("${spring.cloud.aws.s3.path.feed}")
     private String feedPath;
 
-    public String uploadFile(UploadPath uploadPath, MultipartFile file) {
-        return uploadFile(uploadPath, null, file);
-    }
-
     /**
-     * subDirectory를 지정하여 S3에 파일을 업로드한다.
-     * 예: uploadFile(MEMBER_PROFILE, "42", file) → member/profile/42/uuid_timestamp.ext
+     * subDirectory를 지정하여 S3에 파일을 업로드한다. 예: uploadFile(MEMBER_PROFILE, "42", file) →
+     * member/profile/42/uuid_timestamp.ext
      */
     public String uploadFile(UploadPath uploadPath, String subDirectory, MultipartFile file) {
 
